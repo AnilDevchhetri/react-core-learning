3ad67f989802776c485113ebee82a82e1afb02fd
AnilDevchhetri <chhetrivision@gmail.com>
2025-09-13 16:05:22 +0900

    sode ,emi

--- pokemon-app/src/App.jsx
+++ pokemon-app/src/App.jsx
@@ -7,11 +7,15 @@ import { useState } from "react"
 function App() {
   //default pokemn is first pokemno in our list
   const [selectedPokemon, setSelectedPokemon] = useState(0);
+  const [showSideMenu, setShowSideMenu] = useState(false)
+  function hanldeToggleMenu() {
+    setShowSideMenu(!showSideMenu);
+  }
   return (
     <>
       {/* header is for small deivces and sidenav is for full size devices */}
-      <Header />
-      <SideNav selectedPokemon={selectedPokemon} setSelectedPokemon={setSelectedPokemon} />
+      <Header hanldeToggleMenu={hanldeToggleMenu} />
+      <SideNav selectedPokemon={selectedPokemon} setSelectedPokemon={setSelectedPokemon} hanldeToggleMenu={hanldeToggleMenu} showSideMenu={showSideMenu} />
       <PokeCard selectedPokemon={selectedPokemon} />
     </>
   )
--- pokemon-app/src/components/Header.jsx
+++ pokemon-app/src/components/Header.jsx
@@ -1,5 +1,11 @@
-export default function Header() {
+export default function Header(props) {
+    const { hanldeToggleMenu } = props
     return (
-        <header>Header</header>
+        <header>
+            <button onClick={hanldeToggleMenu} className="open-nav-button">
+                <i className="fa-solid fa-bars"></i>
+            </button>
+            <h1 className="text-gradient">Pokedex</h1>
+        </header>
     )
 }
